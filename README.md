b912bbd262c37a2f7c9933ea3a6427d337496007
Hercilio <contato@hercilio.me>
2021-12-04 21:04:55 -0300

    Git inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+# Projeto de PDV simples para estudo em Laravel.
+
+## projeto somente para estudo, aprimorando minha experiência com Laravel e estudos de POO com PHP
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page')
+
+@section('title', 'Home')
+
+@section('content_header')
+    <h1>Home</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+    
+    @include('dashboard')
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
